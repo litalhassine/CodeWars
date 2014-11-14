8838a07c6842dc8dedb1c3c2d2482aa22d92223a
Lital, Hassine <litalhassine@gmail.com>
2014-11-14 10:38:54 +0200

    vector class submmitted version

--- vector_class.py
+++ vector_class.py
@@ -1,19 +1,24 @@
+import math
+
 class Vector:
 
     def __init__(self,arr):
         self.arr = arr
 
     def __str__(self):
-        return str(self.arr)
+        return filter(lambda x: not x.isspace(),str(tuple(self.arr)))
 
     def add(self, other):
         return Vector([x+y for x,y in zip(self.arr,other.arr)])
 
-    def substract(self,other):
+    def subtract(self,other):
         return Vector([x-y for x,y in zip(self.arr,other.arr)])
 
     def dot(self,other):
-        return Vector(reduce(lambda x,y: x+y,[x*y for x,y in zip(self.arr,other.arr)]))
+        return reduce(lambda x,y: x+y,[x*y for x,y in zip(self.arr,other.arr)])
+
+    def norm(self):
+        return math.sqrt(reduce(lambda x,y: x+y,map(lambda x: x*x, self.arr)))
     
     def equals(self,other):
         return self.arr == other.arr
